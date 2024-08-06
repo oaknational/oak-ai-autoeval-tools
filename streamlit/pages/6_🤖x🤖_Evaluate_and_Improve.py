01bf4cc302ae6abb278920ac95e6c18542d02870
ikaankeskin <ikaankeskin@gmail.com>
2024-08-06 11:56:33 +0100

    better ui flow

--- streamlit/pages/6_🤖x🤖_Evaluate_and_Improve.py
+++ streamlit/pages/6_🤖x🤖_Evaluate_and_Improve.py
@@ -10,6 +10,11 @@ import os
 from utils import  log_message, new_sample
 from openai import OpenAI
 import json
+import matplotlib.pyplot as plt
+import warnings
+
+
+warnings.filterwarnings("ignore", category=UserWarning, message="pandas only supports SQLAlchemy")
 
 
 load_dotenv()
@@ -90,6 +95,37 @@ def fetch_final_data(lesson_plan_id, prompt_id, experiment_id):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+    
+def delete_created_sample(sample_id):
+    try:
+        conn = get_db_connection()
+        query = """
+        DELETE FROM public.m_samples
+        WHERE id = %s
+        """
+        cur = conn.cursor()
+        cur.execute(query, (sample_id,))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"An error occurred while deleting the sample: {e}")
+        return False
+def delete_lesson_plans_from_sample_lesson_plans(sample_id):
+    try:
+        conn = get_db_connection()
+        query = """
+        DELETE FROM public.m_sample_lesson_plans
+        WHERE sample_id = %s
+        """
+        cur = conn.cursor()
+        cur.execute(query, (sample_id,))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"An error occurred while deleting the sample lesson plans: {e}")
+        return False
 
 def run_agent_openai_inference(prompt, llm_model, llm_model_temp, timeout=150):
         client = OpenAI( api_key= os.environ.get("OPENAI_API_KEY"), timeout=timeout)
@@ -122,57 +158,123 @@ def run_agent_openai_inference(prompt, llm_model, llm_model_temp, timeout=150):
             }
 # Fetch the data
 lessons_df = fetch_bad_lesson_plans()
+
 if lessons_df is None or lessons_df.empty:
-    st.error("Failed to fetch lesson plans. Please check the database connection or query.")
-    st.stop()  # Stop further execution if DataFrame is invalid
-
-
-if lessons_df is not None:
     lessons_df = lessons_df.astype({
     'min_result': 'float64',
     'max_result': 'float64',
 
     })
+    st.error("Failed to fetch lesson plans. Please check the database connection or query.")
+    st.stop()  # Stop further execution if DataFrame is invalid
 
-    st.header("Evaluate and Improve Lesson Plans")
-    st.write("This page allows you to evaluate and improve lesson plans that have received low scores. Below are the lesson plans that have received low scores. Select a row to view the details and improve the lesson plan.")
+#seesion state selected_plan none
+if 'selected_plan' not in st.session_state:
+    st.session_state.selected_plan = None
 
-    # Display the data frame
-    st.dataframe(lessons_df[['generation_details', 'prompt_title', 'min_result', 'max_result', 'justification_count', 'score_1_count', 'score_2_count', 'score_3_count', 'score_4_count', 'score_5_count']])
+if 'justifications' not in st.session_state:
+    st.session_state.justifications = None
+
+# Filtering options
+st.sidebar.header("Filter Options")
+score_threshold = st.sidebar.slider("Max Score Threshold", 1, 5, 2)
+filtered_df = lessons_df[lessons_df['max_result'] <= score_threshold]
+st.sidebar.write("Filtered Lesson Plans")
+st.sidebar.dataframe(filtered_df)
+# Expandable sections for lesson details
+st.write("## Lesson Details")
+for i, row in filtered_df.iterrows():
+    with st.expander(f"Lesson {i + 1}: {row['prompt_title']}"):
+        col1, col2 = st.columns(2)
+
+        # Add content to the first column
+        with col1:
+            # st.header("Column 1")
+            st.write(f"**Generation Details:** {row['generation_details']}")
+            st.write(f"**Total Experiments Run:** {row['justification_count']}")
+            # st.header("Column 2")
+            st.write(f"**Max Result:** {row['max_result']}")
+            st.write(f"**Min Result:** {row['min_result']}")
+            # Score Distribution
+            st.write("## Score Distribution")
+            score_columns = ['score_1_count', 'score_2_count', 'score_3_count', 'score_4_count', 'score_5_count']
+            score_totals = row[score_columns]
+
+            # Plot the distribution of scores
+            fig, ax = plt.subplots()
+            ax.bar(score_columns, score_totals, color=['#ff9999', '#66b3ff', '#99ff99', '#ffcc99', '#c2c2f0'])
+            ax.set_title("Score Distribution")
+            ax.set_ylabel("Number of Scores")
+            st.pyplot(fig)
+            
+            
+            
+
+        # Add content to the second column
+        with col2:
+            
+            justifications = None
+            # Fetch justifications for the selected row
+            justifications = fetch_result_data(row['lesson_plan_id'], row['prompt_id'], row['min_result'])
+            st.write('Models Justification for the lowest score')
+            st.write("Justification:", justifications['justification'].values[0])
+        
+        unique_key = f"select_button_{i}_{row['lesson_plan_id']}"
+        if st.button("Select Lesson Plan", key=unique_key):
+            st.success(f"Lesson Plan {i + 1} selected!")
+            st.session_state.selected_plan= row['lesson_plan_id']
+            st.session_state.justifications = justifications['justification'].values[0]
+        
+        
+
+lessons_df=filtered_df
 
 
-    selected_index = st.selectbox("Select a row to perform an action", lessons_df.index, index=1)
-    selected_row = lessons_df.loc[selected_index]
-
-    st.write("You selected:")
-    st.write(selected_row)
+if st.session_state.selected_plan is not None:
+# Fetch the selected row using session_state.selected_plan
+    selected_row = lessons_df[lessons_df['lesson_plan_id'] == st.session_state.selected_plan]
+    selected_row = selected_row.iloc[0]
 
     lesson_plan = json.loads(selected_row['lesson_plan_json'])
 
-    
+
     prompt_details =None
-    justifications = None
+    
 
     prompt_id = selected_row['prompt_id']
     prompt_details = get_prompt(prompt_id)
     # st.write("Lesson Plan:", lesson_plan)
     if prompt_details is not None:
         
-       
+        
         with st.expander('Prompt Details'):
             st.write("Prompt Details:", prompt_details)
-    # Fetch justifications for the selected row
-    justifications = fetch_result_data(selected_row['lesson_plan_id'], selected_row['prompt_id'], selected_row['min_result'])
-
-    if justifications is not None:
-        with st.expander('Models Justification for low score'):
-            st.write("Justification:", justifications['justification'].values[0])
-
+   
     llm_model = 'gpt-4o'
     llm_model_temp = 0.5
-    
 
-    prompt = selected_row['lesson_plan_json'] +'test'+'\n'+ 'Above lesson plan has received the following review:\n ' + justifications['justification'].values[0] + '\n Please make necessary changes to improve the lesson plan. Adhere to the original formatting and just retrn the json of the lesson plan. Do not include your reasoining in your response. You should respond with a valid JSON document where each key of the object corresponds with the keys of the lesson plan. The value of each key should be the content for that part of the lesson plan. And avoid introducing line break characters. \n'
+
+    improvement_prompt = (
+    "You are an expert lesson plan improvement agent." 
+    +"You will be provided with a lesson plan that has received a low score due to failing to meet the required standards. "
+    +"Your task is to improve the lesson plan by making necessary changes to improve the lesson plan.\n\n"
+    + "Here is the lesson plan:\n\n  "
+    + selected_row['lesson_plan_json']
+    +"The lesson plan has received the following review:\n\n  "
+    + st.session_state.justifications
+    + "\n\n  Please make necessary changes to improve the lesson plan. "
+    + "Adhere to the original formatting and just return the json of the lesson plan. "
+    + "Do not include your reasoning in your response. "
+    + "Only edit the parts of the lesson plan that need improvement based on the review. "
+    + "You should respond only with a valid JSON document. "
+    + "Ensure that each key in the JSON corresponds exactly to the keys in the provided lesson plan. "
+    + "Do not alter the formatting in any way. "
+    + "Avoid introducing line break characters.\n\n  "
+    
+    
+)
+
+    st.markdown(improvement_prompt)
     sample_title = 'temp_sample'
     created_by = 'improve_lesson_plan_page'
     experiment_id = None
@@ -185,6 +287,9 @@ if lessons_df is not None:
     exp_description = 'temp_experiment'
     teacher_id = 'c2358325-bf3c-44e8-80d9-37b445dad389'
     tracked= False
+    llm_model_temp = 0.5
+    llm_model = 'gpt-4o'
+    
     # experiment_id = None
 
     if st.button("Perform Improvement"):
@@ -194,7 +299,7 @@ if lessons_df is not None:
             st.write("Sample created successfully!")
 
             st.write("Running Improvement")
-            response_n = run_agent_openai_inference(prompt, llm_model, llm_model_temp, timeout=150)
+            response_n = run_agent_openai_inference(improvement_prompt, llm_model, llm_model_temp, timeout=150)
             if response_n['response'] is not None:
                 st.write("Response")
                 with st.expander('Response'):
@@ -219,27 +324,38 @@ if lessons_df is not None:
                         st.success("Running Evaluation")
                         sample_ids = [sample_id]
                         prompt_ids = [prompt_id]
-                        experiment_id = start_experiment(
-                        experiment_name, exp_description, sample_ids, teacher_id,
-                        prompt_ids, st.session_state.limit, st.session_state.llm_model,
-                        tracked, st.session_state.llm_model_temp)
+                        experiment_id = start_experiment(experiment_name, exp_description, sample_ids, teacher_id,
+                        prompt_ids, limit, llm_model,
+                        tracked, llm_model_temp)
                     
                 
                         if experiment_id is not None:
                             st.success("Experiment completed successfully!")
                             result = fetch_final_data(lesson_plan_id, prompt_id, experiment_id)
-                            
+
+                            # Ensure both values are float for comparison
+                            new_result = float(result['result'].values[0])
+                            previous_result = selected_row['min_result']
+
                             st.write('New Result:')
-                            st.write(result['result'].values[0])
+                            st.write(new_result)
                             st.write('Justification:')
                             st.write(result['justification'].values[0])
 
                             st.write('Previous Result:')
-                            st.write(selected_row['min_result'])
+                            st.write(previous_result)
                             st.write('Justification:')
                             st.write(justifications['justification'].values[0])
-
-                            if result['result'].values[0] > selected_row['min_result']:
+                            delete_created_sample(sample_id)
+                            delete_lesson_plans_from_sample_lesson_plans(sample_id)
+                            if new_result > previous_result:
                                 st.success("Lesson plan improved successfully!")
+                            elif previous_result > new_result:
+                                st.error("Lesson plan has been made worse!")
+                            else:
+                                st.warning("No improvement detected!")
 
-       
+                            # if result['result'].values[0] > selected_row['min_result']:
+                            #     st.success("Lesson plan improved successfully!")
+
+    
